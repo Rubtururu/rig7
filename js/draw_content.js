e44fcd7f9bc415ec0e9568730e2c1130b2bd5614
Rubtururu <59855904+Rubtururu@users.noreply.github.com>
2024-06-23 03:26:23 +0200

    Update draw_content.js

--- js/draw_content.js
+++ js/draw_content.js
@@ -114,10 +114,10 @@ function update_ico()
     }
 
 
-    let personal_pct = precisionRound(game.ico_personal_share/game.ico_data_pot*10,2);
+    let personal_pct = precisionRound(game.ico_personal_share/game.ico_data_pot*100,2);
   
     // Personal ICO
-    $('.ico_pot_yours').html('Your investment so far: '+precisionRound(web3.fromWei(game.ico_personal_share,'ether'),4)+'<i class="fab fa-ethereum"></i> ('+personal_pct+'%)');
+    $('.ico_pot_yours').html('Your investment so far: '+precisionRound(web3.fromWei(game.ico_personal_share,'ether'),0.1)+'<i class="fab fa-ethereum"></i> ('+personal_pct+'%)');
 
 }
 
